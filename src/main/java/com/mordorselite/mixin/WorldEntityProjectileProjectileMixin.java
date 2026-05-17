1bd8105d264962a6a140bb64bd098c27a131d58d
fhoelz <kachelstacktus@gmail.com>
2026-05-17 21:37:27 +0200

    Commiting Working Inject Target in case I want to go back to it

--- src/main/java/com/mordorselite/mixin/WorldEntityProjectileProjectileMixin.java
+++ src/main/java/com/mordorselite/mixin/WorldEntityProjectileProjectileMixin.java
@@ -1,30 +1,25 @@
 package com.mordorselite.mixin;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.projectile.throwableitemprojectile.ThrownEnderpearl;
+import net.minecraft.world.item.ItemStack;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Projectile.class)
 public class WorldEntityProjectileProjectileMixin {
 
-    @Redirect(
-        method = "spawnProjectileUsingShoot(Lnet/minecraft/world/entity/projectile/ProjectileFactory;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/entity/LivingEntity;DDDFF)Lnet/minecraft/world/entity/projectile/Projectile;",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/projectile/Projectile;shoot(DDDFF)V")
+    @Inject(
+        method = "shoot",
+        at = @At(value = "INVOKE")
     )
-    private static void redirectShootFactory(Projectile projectile, double x, double y, double z, float power, float uncertainty) {
-        float actualUncertainty = projectile instanceof ThrownEnderpearl ? 0 : uncertainty;
-        projectile.shoot(x, y, z, power, actualUncertainty);
-    }
-
-    @Redirect(
-        method = "spawnProjectileUsingShoot(Lnet/minecraft/world/entity/projectile/Projectile;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/item/ItemStack;DDDFF)Lnet/minecraft/world/entity/projectile/Projectile;",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/projectile/Projectile;shoot(DDDFF)V")
-    )
-    private static void redirectShootDirect(Projectile projectile, double x, double y, double z, float power, float uncertainty) {
-        float actualUncertainty = projectile instanceof ThrownEnderpearl ? 0 : uncertainty;
-        projectile.shoot(x, y, z, power, actualUncertainty);
+    private static void removeEnderpearlUncertainty(final double xd, final double yd, final double zd, final float pow, final float uncertainty, CallbackInfo ci) {
+        double neverUsed = xd + yd;
     }
 }
